6e383d45f90040a6f0b94955f6428f62fb6e9d33
Arthurfu974 <arthur.lecocq974@gmail.com>
2020-02-13 12:41:42 +0400

    flamme2

--- src/driver/capteur_flamme/capteur_flamme.cpp
+++ src/driver/capteur_flamme/capteur_flamme.cpp
@@ -1,8 +1,34 @@
 #include "Arduino.h"
 #include "flamme.h"
 
-
+int digitalPin =  ;
+int digitalVal;
+int analogVal; 
 
 void flamme_init(void){
   Serial.println("init flamme");
+
+  pinMode(digitalPin, INPUT);
+  Serial.begin(9600);
+  
+}
+
+void loop() {
+
+   digitalVal = digitalRead(digitalPin);
+
+        if(digitalVal == HIGH)   //si la flamme est détecté
+
+        { 
+          Serial.println("Flamme détecté"); 
+        }
+
+        else 
+        {
+          Serial.println("Flamme non détecté");
+        }
+
+analogVal = analogRead(analogPin); 
+Serial.println(analogVal);
+
 }
